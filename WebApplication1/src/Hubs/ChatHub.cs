910e3871543b9fb88f8d0264f66cbea1bc5568f1
Daniil Tolstov <derexdahik@yandex.ru>
2019-03-20 01:50:20 +0300

    <master> Add new methods to ChatHub
    Add GetUsersInRoom method
    Add GetAvailableRooms method

--- WebApplication1/src/Hubs/ChatHub.cs
+++ WebApplication1/src/Hubs/ChatHub.cs
@@ -12,14 +12,15 @@ namespace WebApplication1.Hubs
     [Authorize]
     public class ChatHub : Hub
     {
-        private static ConcurrentDictionary<string, List<string>> UserInRooms = new ConcurrentDictionary<string, List<string>>();
-        private static ConcurrentDictionary<string, List<string>> MessagesInRoom = new ConcurrentDictionary<string, List<string>>();
+        private static ConcurrentDictionary<string, HashSet<string>> _usersOfRoom = new ConcurrentDictionary<string, HashSet<string>>();
+        private static ConcurrentDictionary<string, HashSet<string>> _userInRooms = new ConcurrentDictionary<string, HashSet<string>>();
+        private static ConcurrentDictionary<string, HashSet<string>> _messagesInRoom = new ConcurrentDictionary<string, HashSet<string>>();
 
         public override async Task OnConnectedAsync()
         {
             var username = UserInfoProvider.GetUsername(Context);
 
-            var roomsOfUser = UserInRooms.GetOrAdd(username, new List<string>());
+            var roomsOfUser = _userInRooms.GetOrAdd(username, new HashSet<string>());
             foreach (var roomId in roomsOfUser)
             {
                 await Groups.AddToGroupAsync(Context.ConnectionId, roomId);
@@ -35,7 +36,7 @@ namespace WebApplication1.Hubs
             var hasJoinedMsg = $"{username} has joined the room.\n";
             var connId = Context.ConnectionId;
 
-            var roomsOfUser = UserInRooms.GetValueOrDefault(username);
+            var roomsOfUser = _userInRooms.GetValueOrDefault(username);
             if (roomsOfUser.Count == 50)
             {
                 await Clients.Caller.SendAsync("roomLimitExceeded");
@@ -43,6 +44,7 @@ namespace WebApplication1.Hubs
             }
 
             roomsOfUser.Add(roomId);
+            _usersOfRoom.GetOrAdd(roomId, new HashSet<string>()).Add(username);
             await Groups.AddToGroupAsync(connId, roomId);
             await Clients.Group(roomId).SendAsync("sendMessage", hasJoinedMsg);
             await SendAllRoomMessagesToCaller(roomId);
@@ -54,7 +56,10 @@ namespace WebApplication1.Hubs
             var hasLeftMsg = $"{username} has left the room.\n";
             var connId = Context.ConnectionId;
 
-            UserInRooms.GetValueOrDefault(username).Remove(roomId);
+            var usersRooms = _userInRooms.GetValueOrDefault(username);
+            Check.Value(usersRooms.Contains(roomId)).IsTrue("User is not present in specified room");
+            usersRooms.Remove(roomId);
+            _usersOfRoom.GetValueOrDefault(roomId).Remove(username);
             await Groups.RemoveFromGroupAsync(connId, roomId);
             await Clients.Group(roomId).SendAsync("sendMessage", hasLeftMsg);
         }
@@ -63,15 +68,31 @@ namespace WebApplication1.Hubs
         {
             var username = UserInfoProvider.GetUsername(Context);
             var msgWithAuthor = $"{username}: {msg}\n";
-            Check.Value(UserInRooms[username].Contains(roomId)).EqualsTo(true, "User is not present in specified room");
+            Check.Value(_userInRooms[username].Contains(roomId)).EqualsTo(true, "User is not present in specified room");
 
             await Clients.Group(roomId).SendAsync("sendMessage", msgWithAuthor);
-            MessagesInRoom.GetValueOrDefault(roomId).Add(msg);
+            _messagesInRoom.GetValueOrDefault(roomId).Add(msg);
+        }
+
+        public async Task GetAvailableRooms()
+        {
+            var availableRooms = new List<string>();
+            foreach (var pair in _usersOfRoom)
+            {
+                availableRooms.Add(pair.Key);
+            }
+            await Clients.Caller.SendAsync("getAvailableRooms", availableRooms);
+        }
+
+        public async Task GetUsersInRoom(string roomId)
+        {
+            var users = _usersOfRoom.GetOrAdd(roomId, new HashSet<string>());
+            await Clients.Caller.SendAsync("getUsersInRoom", users);
         }
 
         private async Task SendAllRoomMessagesToCaller(string roomId)
         {
-            var roomMessages = MessagesInRoom.GetOrAdd(roomId, new List<string>());
+            var roomMessages = _messagesInRoom.GetOrAdd(roomId, new HashSet<string>());
             if (roomMessages.Count>0)
             {
                 await Clients.Caller.SendAsync("roomMessages", roomMessages);
